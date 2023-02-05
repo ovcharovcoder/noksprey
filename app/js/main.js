80c0ba887799e28670b56f66e32efc5057d0851e
Andrei Ovcharov <datoshcode@gmail.com>
2023-02-05 11:53:02 +0200

    added parallax to about section

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,17 @@
-$(function () {});
+//Parallax img
+let bg1 = document.querySelector("#parallaxImg1");
+window.addEventListener("mousemove", function (e) {
+  let x = e.clientX / window.innerWidth;
+  let y = e.clientY / window.innerHeight;
+  bg1.style.transform = "translate(-" + x * 8 + "px, -" + y * 8 + "px)";
+});
+
+let bg2 = document.querySelector("#parallaxImg2");
+window.addEventListener("mousemove", function (e) {
+  let x = e.clientX / window.innerWidth;
+  let y = e.clientY / window.innerHeight;
+  bg2.style.transform = "translate(-" + x * 8 + "px, -" + y * 8 + "px)";
+});
+
+//WOW
+new WOW().init();
