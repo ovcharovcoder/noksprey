101d266f21b000d8f523b2a4c6062fb46dc290a6
Andrei Ovcharov <datoshcode@gmail.com>
2023-02-09 21:42:39 +0200

    completion of the adaptive version

--- app/js/main.js
+++ app/js/main.js
@@ -43,4 +43,27 @@ window.addEventListener("mousemove", function (e) {
 });
 
 //WOW
-new WOW().init();
+var wow = new WOW({
+  boxClass: "wow", // animated element css class (default is wow)
+  animateClass: "animated", // animation css class (default is animated)
+  offset: 40, // distance to the element when triggering the animation (default is 0)
+  mobile: true, // trigger animations on mobile devices (default is true)
+  live: true, // act on asynchronously loaded content (default is true)
+  callback: function (box) {
+    // the callback is fired every time an animation is started
+    // the argument that is passed in is the DOM node being animated
+  },
+  scrollContainer: null, // optional scroll container selector, otherwise use window,
+  resetAnimation: true, // reset animation on end (default is true)
+});
+wow.init();
+
+// Burger menu
+const mobileNavButton = document.querySelector(".mobile-nav-button");
+const mobileNavIcon = document.querySelector(".mobile-nav-button__icon");
+const mobileNav = document.querySelector(".mobile-nav");
+mobileNavButton.addEventListener("click", function () {
+  mobileNavIcon.classList.toggle("active");
+  mobileNav.classList.toggle("active");
+  document.body.classList.toggle("no-scroll");
+});
